0e98c64e0044707d36f538a606d7d15cb648bf91
MrPandir <MrPandir@users.noreply.github.com>
2025-03-13 19:06:06 +0100

    feat: add !rm command for track removal from queue

--- src/bot/commands/index.ts
+++ src/bot/commands/index.ts
@@ -1,8 +1,10 @@
 import type { CommandExecute } from "../types";
 import sr from "./sr";
 import song from "./song";
+import rm from "./rm";
 
 export const commands: Record<string, CommandExecute> = {
   sr: sr.execute,
   song: song.execute,
+  rm: rm.execute,
 };
---
+++ src/bot/commands/rm.ts
@@ -0,0 +1,55 @@
+import { client } from "@bot/client";
+import { Track } from "@entities/track";
+import { queue } from "@services/queue";
+import { Command } from "../types";
+
+// TODO: Implement deletion by spotify link
+
+async function sayTrackDeleted(parentMessageId: string, track: Track | null) {
+  if (!track) {
+    await client.reply(parentMessageId, "Track not found");
+    return;
+  }
+
+  await client.reply(parentMessageId, `Track "${track.title}" deleted`);
+}
+
+export const rm: Command = {
+  permission: "USER",
+  async execute({ client, author, args, tags }) {
+    // Check if queue is empty
+    if (queue.getTracksByUser(author.id).length === 0) {
+      await client.reply(tags["id"], "Your queue is empty");
+      return;
+    }
+
+    // Delete last track from queue
+    if (args.length == 0) {
+      const deletedTrack = queue.removeTrack(author.id, 1);
+      await sayTrackDeleted(tags["id"], deletedTrack);
+      return;
+    }
+
+    // Delete track by index from end of queue
+    if (args.length == 1) {
+      const endIndex = Number(args[0]);
+
+      if (!isNaN(endIndex) && isFinite(endIndex)) {
+        const deletedTrack = queue.removeTrack(author.id, endIndex);
+        await sayTrackDeleted(tags["id"], deletedTrack);
+        return;
+      }
+    }
+
+    // Delete track by title or artists from queue
+    if (args.length >= 1) {
+      // TODO: Implement deletion for admin (optional)
+
+      const deletedTrack = queue.removeTrack(author.id, args.join(" "));
+      await sayTrackDeleted(tags["id"], deletedTrack);
+      return;
+    }
+  },
+};
+
+export default rm;
